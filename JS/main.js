a162ee69770f26c0554aff01ea91b2975798e6bb
FrancescoPepi <francescopepi16@gmail.com>
2023-08-21 23:21:52 +0200

    Add Bonus

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,9 @@
 const playButton = document.getElementById("play-game");
 const cellContainer = document.getElementById("cell-group");
-const difficulty = document.getElementById("difficulty").value;
-// let difficultyValue = difficulty.options;
-console.log(typeof difficulty);
-// let cellsTotal = 10 * 10;
 
 playButton.addEventListener("click", function () {
-  let cellsTotalDifficulty = difficultySelect(difficulty);
+  const difficulty = document.getElementById("difficulty").value;
+  const cellsTotalDifficulty = difficultySelect(difficulty);
   console.log(cellsTotalDifficulty + "prova");
   gridGenerate(cellsTotalDifficulty, cellContainer);
 });
@@ -14,16 +11,22 @@ playButton.addEventListener("click", function () {
 function gridGenerate(cellsTotal, cellsContainer) {
   cellsContainer.innerHTML = "";
   for (let i = 1; i <= cellsTotal; i++) {
-    cellCreation(cellsContainer, i);
+    cellCreation(cellsTotal, i);
   }
 }
 
-function cellCreation(cellsContainer, i) {
+function cellCreation(cellsTotal, i) {
   //   const randomNumber = Math.floor(Math.random() * 100);
   const cell = document.createElement("li");
   cell.setAttribute("data-index", i);
   cell.classList.add("cell");
-  cell.classList.add("lg-10");
+  if (cellsTotal < 81) {
+    cell.classList.add("lg-7");
+  } else if (cellsTotal > 81) {
+    cell.classList.add("lg-10");
+  } else {
+    cell.classList.add("lg-9");
+  }
   cell.addEventListener("click", function () {
     if (cell.innerText == "") {
       const index = parseInt(this.getAttribute("data-index"));
@@ -40,17 +43,16 @@ function cellCreation(cellsContainer, i) {
 }
 
 function difficultySelect(difficulty) {
-  let cellsTotal;
+  let cellsTotal = 10 * 10;
 
-  if (difficulty == "2") {
+  if (difficulty == 2) {
     cellsTotal = 9 * 9;
     console.log(cellsTotal);
-  } else if (difficulty == "3") {
+    return cellsTotal;
+  } else if (difficulty == 3) {
     cellsTotal = 7 * 7;
     console.log(cellsTotal);
-  } else {
-    cellsTotal = 10 * 10;
+    return cellsTotal;
   }
-  //   console.log(cellsTotal);
   return cellsTotal;
 }
